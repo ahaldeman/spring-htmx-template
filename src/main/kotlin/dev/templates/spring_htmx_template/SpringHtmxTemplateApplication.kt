61fc8795daa8d130029fe7d9a42c74af805c1fe9
Alex Haldeman <alexhldmn5@gmail.com>
2024-05-29 22:45:08 -0400

    Add initial spring project

---
+++ src/main/kotlin/dev/templates/spring_htmx_template/SpringHtmxTemplateApplication.kt
@@ -0,0 +1,11 @@
+package dev.templates.spring_htmx_template
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringHtmxTemplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringHtmxTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/dev/templates/spring_htmx_template/SpringHtmxTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.templates.spring_htmx_template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringHtmxTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
